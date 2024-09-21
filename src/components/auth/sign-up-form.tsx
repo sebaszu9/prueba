9efb4db374b8f608ebb034fe278b3f2b043ff848
Juan Sebastian Murcia <smurcia0820@gmail.com>
2024-09-21 12:09:07 -0500

    first commit

---
+++ src/components/auth/sign-up-form.tsx
@@ -0,0 +1,166 @@
+'use client';
+
+import * as React from 'react';
+import RouterLink from 'next/link';
+import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import Alert from '@mui/material/Alert';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormHelperText from '@mui/material/FormHelperText';
+import InputLabel from '@mui/material/InputLabel';
+import Link from '@mui/material/Link';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import { Controller, useForm } from 'react-hook-form';
+import { z as zod } from 'zod';
+
+import { paths } from '@/paths';
+import { authClient } from '@/lib/auth/client';
+import { useUser } from '@/hooks/use-user';
+import { MenuItem, Select } from '@mui/material';
+
+const schema = zod.object({
+  userName: zod.string().min(1, { message: 'Last name is required' }),
+  email: zod.string().min(1, { message: 'Email is required' }).email(),
+  password: zod.string().min(6, { message: 'Password should be at least 6 characters' }),
+  terms: zod.boolean().refine((value) => value, 'You must accept the terms and conditions'),
+  userType: zod.string().min(1, { message: 'User type is required' })
+});
+
+type Values = zod.infer<typeof schema>;
+
+const defaultValues = {userType: '', userName: '', email: '', password: '', terms: false } satisfies Values;
+
+export function SignUpForm(): React.JSX.Element {
+  const router = useRouter();
+
+  const { checkSession } = useUser();
+
+  const [isPending, setIsPending] = React.useState<boolean>(false);
+
+  const {
+    control,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<Values>({ defaultValues, resolver: zodResolver(schema) });
+
+  const onSubmit = React.useCallback(
+    async (values: Values): Promise<void> => {
+      setIsPending(true);
+
+      const { error } = await authClient.signUp(values);
+
+      if (error) {
+        setError('root', { type: 'server', message: error });
+        setIsPending(false);
+        return;
+      }
+
+      // Refresh the auth state
+      await checkSession?.();
+
+      // UserProvider, for this case, will not refresh the router
+      // After refresh, GuestGuard will handle the redirect
+      router.refresh();
+    },
+    [checkSession, router, setError]
+  );
+
+  return (
+    <Stack spacing={3}>
+      <Stack spacing={1}>
+        <Typography variant="h4">Sign up</Typography>
+        <Typography color="text.secondary" variant="body2">
+          Already have an account?{' '}
+          <Link component={RouterLink} href={paths.auth.signIn} underline="hover" variant="subtitle2">
+            Sign in
+          </Link>
+        </Typography>
+      </Stack>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={2}>
+        <Controller
+          control={control}
+          name="userType"
+          render={({ field }) => (
+            <FormControl error={Boolean(errors.userType)}>
+              <InputLabel>Tipo de usuario</InputLabel>
+                <Select
+                  {...field}
+                  label="Tipo de usuario"
+                >
+                  <MenuItem value="administrador">Administrador</MenuItem>
+                  <MenuItem value="almacen">Usuario de almacén</MenuItem>
+                  <MenuItem value="vendedor">Vendedor</MenuItem>
+                  <MenuItem value="gestor">Gestor alimentario</MenuItem>
+                </Select>
+                {errors.userType ? (
+                  <FormHelperText>{errors.userType.message}</FormHelperText>
+                ) : null}
+              </FormControl>
+           )}
+          />
+          <Controller
+            control={control}
+            name="userName"
+            render={({ field }) => (
+              <FormControl error={Boolean(errors.userName)}>
+                <InputLabel>Username</InputLabel>
+                <OutlinedInput {...field} label="User name" />
+                {errors.userName ? <FormHelperText>{errors.userName.message}</FormHelperText> : null}
+              </FormControl>
+            )}
+          />
+          <Controller
+            control={control}
+            name="email"
+            render={({ field }) => (
+              <FormControl error={Boolean(errors.email)}>
+                <InputLabel>Email address</InputLabel>
+                <OutlinedInput {...field} label="Email address" type="email" />
+                {errors.email ? <FormHelperText>{errors.email.message}</FormHelperText> : null}
+              </FormControl>
+            )}
+          />
+          <Controller
+            control={control}
+            name="password"
+            render={({ field }) => (
+              <FormControl error={Boolean(errors.password)}>
+                <InputLabel>Password</InputLabel>
+                <OutlinedInput {...field} label="Password" type="password" />
+                {errors.password ? <FormHelperText>{errors.password.message}</FormHelperText> : null}
+              </FormControl>
+            )}
+          />
+          <Controller
+            control={control}
+            name="terms"
+            render={({ field }) => (
+              <div>
+                <FormControlLabel
+                  control={<Checkbox {...field} />}
+                  label={
+                    <React.Fragment>
+                      I have read the <Link>terms and conditions</Link>
+                    </React.Fragment>
+                  }
+                />
+                {errors.terms ? <FormHelperText error>{errors.terms.message}</FormHelperText> : null}
+              </div>
+            )}
+          />
+          {errors.root ? <Alert color="error">{errors.root.message}</Alert> : null}
+          <Button disabled={isPending} type="submit" variant="contained">
+            Sign up
+          </Button>
+        </Stack>
+      </form>
+    </Stack>
+  );
+}
